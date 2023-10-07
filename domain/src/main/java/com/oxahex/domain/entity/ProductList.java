79dd17b66afab4f93e7fb5e6550a30c64c4ac9b8
oxahex <oxahex@gmail.com>
2023-10-07 17:22:37 +0900

    chore: 사용 안 하는 엔티티 주석 처리

--- domain/src/main/java/com/oxahex/domain/entity/ProductList.java
+++ domain/src/main/java/com/oxahex/domain/entity/ProductList.java
@@ -1,35 +1,35 @@
-package com.oxahex.domain.entity;
-
-import com.oxahex.domain.converter.OrganizationCodeConverter;
-import com.oxahex.domain.type.OrganizationCode;
-import lombok.RequiredArgsConstructor;
-import lombok.ToString;
-
-import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
-
-@Entity
-@Table(name = "PRODUCT_LIST")
-@RequiredArgsConstructor
-@ToString
-public class ProductList {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
-    private Long productListId;
-
-    @Column(name = "org_cd")
-    @Convert(converter = OrganizationCodeConverter.class)
-    private OrganizationCode organizationCode;
-
-
-    @Column(name = "prod_cd")
-    @OneToMany(mappedBy = "productId")
-    private List<ProductInfo> productInfoList = new ArrayList<>();
-
-    public void add(ProductInfo productInfo) {
-        this.productInfoList.add(productInfo);
-    }
-}
+//package com.oxahex.domain.entity;
+//
+//import com.oxahex.domain.converter.OrganizationCodeConverter;
+//import com.oxahex.domain.type.OrganizationCode;
+//import lombok.RequiredArgsConstructor;
+//import lombok.ToString;
+//
+//import javax.persistence.*;
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//@Entity
+//@Table(name = "PRODUCT_LIST")
+//@RequiredArgsConstructor
+//@ToString
+//public class ProductList {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @Column(name = "id")
+//    private Long productListId;
+//
+//    @Column(name = "org_cd")
+//    @Convert(converter = OrganizationCodeConverter.class)
+//    private OrganizationCode organizationCode;
+//
+//
+//    @Column(name = "prod_cd")
+//    @OneToMany(mappedBy = "productId")
+//    private List<ProductInfo> productInfoList = new ArrayList<>();
+//
+//    public void add(ProductInfo productInfo) {
+//        this.productInfoList.add(productInfo);
+//    }
+//}
